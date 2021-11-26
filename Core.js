e5b958b8a356df13d312d73dd8d383fd23ee6e5c
Jens Eeckhout <jenseeckhout2009@hotmail.com>
2021-11-26 10:37:04 +0100

    FillDeco bugfix
    Fixed bugs where it sometimes wouldnt properly fill the plane as intended

--- Core.js
+++ Core.js
@@ -585,7 +585,7 @@ class App {
                 InitialPos.copy(this.reticle.position);
 
                 //CODE TO TEST ON FLAT PLAINS - REMOVE FOR PROPER TESTING
-                //InitialPos.y = 2;
+                //InitialPos.y = 0.5;
 
                 LTPoint.copy(WallframePoints[0].position);
                 LTPoint.y = InitialPos.y;
@@ -628,7 +628,7 @@ class App {
                 InitialPos.copy(this.reticle.position);
 
                 //CODE TO TEST ON FLAT PLAINS - REMOVE FOR PROPER TESTING
-                //InitialPos.y = 2;
+                //InitialPos.y = 0.5;
 
                 LTPoint.copy(DoorPoints[0].position);
                 LTPoint.y = InitialPos.y;
@@ -943,7 +943,7 @@ class App {
         {
                 if (WallPoints.length !== 0)
                 {
-                    let distanceToMarker = WallPoints[WallPoints.length - 2].position.distanceToSquared(this.reticle.position);
+                    let distanceToMarker = WallPoints[WallPoints.length - 1].position.distanceToSquared(this.reticle.position);
                     if (distanceToMarker < MinDistance)
                     {
                         FinishedPlacingWalls = true;
@@ -957,7 +957,7 @@ class App {
                         document.getElementById("WallsIcon").style.display = "none";
                     }
 
-                    distanceToMarker = WallPoints[0].position.distanceToSquared(this.reticle.position);
+                    distanceToMarker = WallPoints[1].position.distanceToSquared(this.reticle.position);
                     if (distanceToMarker < MinDistance)
                     {
                         let Point1;
@@ -1100,7 +1100,7 @@ class App {
                     ConstrainedYPosWalls = WallPoints[1].position.y;
 
                     //DELETE - Just added it now for testing purposes
-                    //ConstrainedYPosWalls = 2;
+                    //ConstrainedYPosWalls = 1.75;
 
                     WallHeight = ConstrainedYPosWalls - WallPoints[0].position.y;
                     this.ResetWallPoints();
@@ -1143,7 +1143,7 @@ class App {
         if (WallframePoints.length === 2)
         {
             //Generate top left
-            //WallframePoints[1].position.y = 2;
+            //WallframePoints[1].position.y = 0.5;
             let topLeftPosition = WallframePoints[0].position.clone();
             topLeftPosition.y = WallframePoints[1].position.y;
             let topLeftSphere = this.CreateSphere(topLeftPosition);
@@ -1205,7 +1205,7 @@ class App {
         if (DoorPoints.length === 2)
         {
             //Generate top left
-            //DoorPoints[1].position.y = 2;
+            //DoorPoints[1].position.y = 0.5;
             let topLeftPosition = DoorPoints[0].position.clone();
             topLeftPosition.y = DoorPoints[1].position.y;
             let topLeftSphere = this.CreateSphere(topLeftPosition);
@@ -2398,6 +2398,7 @@ class App {
                 SpawnedDecorations.push(trimToSpawn)
 
                 //Decrement nr by one seeing as we already spawned one to get the data
+                if (nrToSpawnX > 0)
                 --nrToSpawnX;
 
                 if (IsX)
@@ -2427,6 +2428,8 @@ class App {
                             trimToSpawn2.position.copy(currentPos);
                             if (currY === 0 && currX === 0 )
                             {
+                                if (nrToSpawnX === 0)
+                                    continue;
                                 ++currX;
                             }
                             trimToSpawn2.position.addScaledVector(positionOffset,currX);
